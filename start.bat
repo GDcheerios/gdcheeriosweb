787f05caee9379ea8fcca6183cbcd6f6407fc113
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-05-17 13:11:38 -0500

    added start.bat for windows

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo checking .env
+if exist ".env" (echo "yurp")
+else (
+    echo creating .env file
+    copy ".env.example" ".env"
+)
+
+echo updating images
+docker compose pull
+
+echo starting cluster
+docker compose up -d --remove-orphans
